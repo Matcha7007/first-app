886ebb8ef3185d5624a51b171e54996b4c5a66cd
Matcha7007 <matchaaddict007@gmail.com>
2023-03-30 13:19:08 +0700

    add components/useravatar

---
+++ src/components/UserAvatar/styles.js
@@ -0,0 +1,15 @@
+import { makeStyles } from "@mui/material/styles";
+
+export default makeStyles(() => ({
+  avatar: {
+    width: 30,
+    height: 30,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: "50%",
+  },
+  text: {
+    color: "white",
+  },
+}));
