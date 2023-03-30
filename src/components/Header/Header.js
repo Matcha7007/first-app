803a10d10dc4f74d504f6a1da75d236d69325bf0
Matcha7007 <matchaaddict007@gmail.com>
2023-03-30 13:10:00 +0700

    add components/header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,213 @@
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  InputBase,
+  Menu,
+  MenuItem
+} from "@mui/material";
+import {
+  Menu as MenuIcon,
+  NotificationsNone as NotificationsIcon,
+  Person as AccountIcon,
+  Search as SearchIcon,
+  ArrowBack as ArrowBackIcon,
+} from "@mui/icons-material";
+import classNames from "classnames";
+
+// styles
+import useStyles from "./styles";
+
+// components
+import { Badge, Typography } from "../Wrappers/Wrappers";
+import Notification from "../Notification/Notification";
+
+// context
+import {
+  useLayoutState,
+  useLayoutDispatch,
+  toggleSidebar,
+} from "../../context/LayoutContext";
+import { useUserDispatch, signOut } from "../../context/UserContext";
+
+const notifications = [
+ 
+  
+];
+
+export default function Header(props) {
+  const classes = useStyles();
+
+  // global
+  const layoutState = useLayoutState();
+  const layoutDispatch = useLayoutDispatch();
+  const userDispatch = useUserDispatch();
+
+  // local
+  const [notificationsMenu, setNotificationsMenu] = useState(null);
+  const [isNotificationsUnread, setIsNotificationsUnread] = useState(true);
+  const [profileMenu, setProfileMenu] = useState(null);
+  const [isSearchOpen, setSearchOpen] = useState(false);
+
+  return (
+    <AppBar position="fixed" className={classes.appBar}>
+      <Toolbar className={classes.toolbar}>
+        <IconButton
+          color="inherit"
+          onClick={() => toggleSidebar(layoutDispatch)}
+          className={classNames(
+            classes.headerMenuButton,
+            classes.headerMenuButtonCollapse,
+          )}
+        >
+          {layoutState.isSidebarOpened ? (
+            <ArrowBackIcon
+              classes={{
+                root: classNames(
+                  classes.headerIcon,
+                  classes.headerIconCollapse,
+                ),
+              }}
+            />
+          ) : (
+            <MenuIcon
+              classes={{
+                root: classNames(
+                  classes.headerIcon,
+                  classes.headerIconCollapse,
+                ),
+              }}
+            />
+          )}
+        </IconButton>
+        <Typography variant="h6" weight="medium" className={classes.logotype}>
+          Code Generation Tool
+        </Typography>
+        <div className={classes.grow} />
+        <div
+          className={classNames(classes.search, {
+            [classes.searchFocused]: isSearchOpen,
+          })}
+        >
+          <div
+            className={classNames(classes.searchIcon, {
+              [classes.searchIconOpened]: isSearchOpen,
+            })}
+            onClick={() => setSearchOpen(!isSearchOpen)}
+          >
+            <SearchIcon classes={{ root: classes.headerIcon }} />
+          </div>
+          <InputBase
+            placeholder="Search…"
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput,
+            }}
+          />
+        </div>
+        <IconButton
+          color="inherit"
+          aria-haspopup="true"
+          aria-controls="mail-menu"
+          onClick={e => {
+            setNotificationsMenu(e.currentTarget);
+            setIsNotificationsUnread(false);
+          }}
+          className={classes.headerMenuButton}
+        >
+          <Badge
+            badgeContent={isNotificationsUnread ? notifications.length : null}
+            color="warning"
+          >
+            <NotificationsIcon classes={{ root: classes.headerIcon }} />
+          </Badge>
+        </IconButton>
+        <IconButton
+          aria-haspopup="true"
+          color="inherit"
+          className={classes.headerMenuButton}
+          aria-controls="profile-menu"
+          onClick={e => setProfileMenu(e.currentTarget)}
+        >
+          <AccountIcon classes={{ root: classes.headerIcon }} />
+        </IconButton>
+        
+        <Menu
+          id="notifications-menu"
+          open={Boolean(notificationsMenu)}
+          anchorEl={notificationsMenu}
+          onClose={() => setNotificationsMenu(null)}
+          className={classes.headerMenu}
+          disableAutoFocusItem
+        >
+          {notifications.map(notification => (
+            <MenuItem
+              key={notification.id}
+              onClick={() => setNotificationsMenu(null)}
+              className={classes.headerMenuItem}
+            >
+              <Notification {...notification} typographyVariant="inherit" />
+            </MenuItem>
+          ))}
+        </Menu>
+        <Menu
+          id="profile-menu"
+          open={Boolean(profileMenu)}
+          anchorEl={profileMenu}
+          onClose={() => setProfileMenu(null)}
+          className={classes.headerMenu}
+          classes={{ paper: classes.profileMenu }}
+          disableAutoFocusItem
+        >
+          <div className={classes.profileMenuUser}>
+            <Typography variant="h4" weight="medium">
+              Dihn
+            </Typography>
+            <Typography
+              className={classes.profileMenuLink}
+              component="a"
+              color="primary"
+             
+            >
+              Dihn
+            </Typography>
+          </div>
+          <MenuItem
+            className={classNames(
+              classes.profileMenuItem,
+              classes.headerMenuItem,
+            )}
+          >
+            <AccountIcon className={classes.profileMenuIcon} /> Profile
+          </MenuItem>
+          <MenuItem
+            className={classNames(
+              classes.profileMenuItem,
+              classes.headerMenuItem,
+            )}
+          >
+            <AccountIcon className={classes.profileMenuIcon} /> Tasks
+          </MenuItem>
+          <MenuItem
+            className={classNames(
+              classes.profileMenuItem,
+              classes.headerMenuItem,
+            )}
+          >
+            <AccountIcon className={classes.profileMenuIcon} /> Messages
+          </MenuItem>
+          <div className={classes.profileMenuUser}>
+            <Typography
+              className={classes.profileMenuLink}
+              color="primary"
+              onClick={() => signOut(userDispatch, props.history)}
+            >
+              Sign Out
+            </Typography>
+          </div>
+        </Menu>
+      </Toolbar>
+    </AppBar>
+  );
+}
