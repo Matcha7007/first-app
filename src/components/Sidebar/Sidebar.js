600695ddb810bc8bc36fd283f99f394e56e2c1af
Matcha7007 <matchaaddict007@gmail.com>
2023-03-30 13:17:42 +0700

    add components/sidebar

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from "react";
+import { Drawer, IconButton, List } from "@mui/material";
+import {
+  Home as HomeIcon,
+  BorderAll as TableIcon,
+  ArrowBack as ArrowBackIcon,
+} from "@mui/icons-material";
+import { useTheme } from "@mui/material/styles";
+import { withRouter } from "react-router-dom";
+import classNames from "classnames";
+
+// styles
+import useStyles from "./styles";
+
+// components
+import SidebarLink from "./components/SidebarLink/SidebarLink";
+
+// context
+import {
+  useLayoutState,
+  useLayoutDispatch,
+  toggleSidebar,
+} from "../../context/LayoutContext";
+
+const structure = [
+  { id: 0, label: "Dashboard", link: "/app/dashboard", icon: <HomeIcon /> },
+
+  { id: 2, label: "Table1", link: "/app/table1", icon: <TableIcon/> },
+
+];
+
+function Sidebar({ location }) {
+  const classes = useStyles();
+  const theme = useTheme();
+
+  // global
+  const { isSidebarOpened } = useLayoutState();
+  const layoutDispatch = useLayoutDispatch();
+
+  // local
+  const [isPermanent, setPermanent] = useState(true);
+
+  useEffect(function() {
+    window.addEventListener("resize", handleWindowWidthChange);
+    handleWindowWidthChange();
+    return function cleanup() {
+      window.removeEventListener("resize", handleWindowWidthChange);
+    };
+  });
+
+  return (
+    <Drawer
+      variant={isPermanent ? "permanent" : "temporary"}
+      className={classNames(classes.drawer, {
+        [classes.drawerOpen]: isSidebarOpened,
+        [classes.drawerClose]: !isSidebarOpened,
+      })}
+      classes={{
+        paper: classNames({
+          [classes.drawerOpen]: isSidebarOpened,
+          [classes.drawerClose]: !isSidebarOpened,
+        }),
+      }}
+      open={isSidebarOpened}
+    >
+      <div className={classes.toolbar} />
+      <div className={classes.mobileBackButton}>
+        <IconButton onClick={() => toggleSidebar(layoutDispatch)}>
+          <ArrowBackIcon
+            classes={{
+              root: classNames(classes.headerIcon, classes.headerIconCollapse),
+            }}
+          />
+        </IconButton>
+      </div>
+      <List className={classes.sidebarList}>
+        {structure.map(link => (
+          <SidebarLink
+            key={link.id}
+            location={location}
+            isSidebarOpened={isSidebarOpened}
+            {...link}
+          />
+        ))}
+      </List>
+    </Drawer>
+  );
+
+  // ##################################################################
+  function handleWindowWidthChange() {
+    const windowWidth = window.innerWidth;
+    const breakpointWidth = theme.breakpoints.values.md;
+    const isSmallScreen = windowWidth < breakpointWidth;
+
+    if (isSmallScreen && isPermanent) {
+      setPermanent(false);
+    } else if (!isSmallScreen && !isPermanent) {
+      setPermanent(true);
+    }
+  }
+}
+
+export default withRouter(Sidebar);
